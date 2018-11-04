f1ed7939583479708719873833c64121e606ec66
Max Fry <m.sukach@innopolis.ru>
2018-11-05 01:05:17 +0300

    v 0.3.2 Changelog: 1. Добавлена глобальная ошибка

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import math
+import numpy as np
 
 from core.graph import draw
 from methods.all import Exact
@@ -10,10 +11,12 @@ from methods.all import ImprovedEuler
 from methods.all import RungeKutta
 
 C = 0.693  # Константа из решения. См. README.md -> Решение
-H = 0.1
+HS = 0.01
+HF = 1.01
+HH = 0.05
 X0 = 0
 Y0 = 1
-XF = 9
+XF = 3
 
 
 def equation(x, y):
@@ -28,29 +31,41 @@ def calculate_error(ys_approx, ys_exact):
     return [abs(y1 - y2) for y1, y2 in zip(ys_approx, ys_exact)]
 
 
+def calculate_mean_error(ys_approx, ys_exact):
+    return sum(calculate_error(ys_approx, ys_exact))/len(ys_exact)
+
+
 if __name__ == '__main__':
-    exact = Exact(solved_equation, H)
-    exact_ys = exact.calculate(X0, Y0, XF)
+    euler_error = []
+    better_euler_error = []
+    rk_error = []
 
-    euler = Euler(equation, H)
-    euler_ys = euler.calculate(X0, Y0, XF)
+    h_axis = np.arange(HS, HF, HH)
+    for H in h_axis:
+        print(H)
+        exact = Exact(solved_equation, H)
+        exact_ys = exact.calculate(X0, Y0, XF)
 
-    better_euler = ImprovedEuler(equation, H)
-    b_euler_ys = better_euler.calculate(X0, Y0, XF)
+        euler = Euler(equation, H)
+        euler_ys = euler.calculate(X0, Y0, XF)
 
-    runge_kutta = RungeKutta(equation, H)
-    rk_ys = runge_kutta.calculate(X0, Y0, XF)
+        better_euler = ImprovedEuler(equation, H)
+        b_euler_ys = better_euler.calculate(X0, Y0, XF)
 
-    #draw(X0, XF, H, ['Exact', 'Euler', 'Improved Euler', 'RungeKutta'], exact_ys, euler_ys, b_euler_ys, rk_ys)
-    draw(X0, XF, H, ['Exact', 'Euler'], exact_ys, euler_ys)
-    draw(X0, XF, H, ['Exact', 'Improved Euler'], exact_ys, b_euler_ys)
-    draw(X0, XF, H, ['Exact', 'Runge Kutta'], exact_ys, rk_ys)
+        runge_kutta = RungeKutta(equation, H)
+        rk_ys = runge_kutta.calculate(X0, Y0, XF)
 
-    euler_error = calculate_error(euler_ys, exact_ys)
-    better_euler_error = calculate_error(b_euler_ys, exact_ys)
-    rk_error = calculate_error(rk_ys, exact_ys)
+        # draw(X0, XF, H, ['Exact', 'Euler'], exact_ys, euler_ys)
+        # draw(X0, XF, H, ['Exact', 'Improved Euler'], exact_ys, b_euler_ys)
+        # draw(X0, XF, H, ['Exact', 'Runge Kutta'], exact_ys, rk_ys)
 
-    draw(X0, XF, H,
-         ['Euler Error', 'Improved Euler Error', 'Runge Kutta Error'],
-         euler_error, better_euler_error, rk_error)
+        euler_error.append(calculate_mean_error(euler_ys, exact_ys))
+        better_euler_error.append(calculate_mean_error(b_euler_ys, exact_ys))
+        rk_error.append(calculate_mean_error(rk_ys, exact_ys))
+
+    draw(HS, HF, HH, ['Euler', 'Impr Euler', 'Runge Kutta'], euler_error, better_euler_error, rk_error)
+
+    #euler_error = calculate_error(euler_ys, exact_ys)
+    #better_euler_error = calculate_error(b_euler_ys, exact_ys)
+    #rk_error = calculate_error(rk_ys, exact_ys)
 
